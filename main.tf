794077642f0aee90010c788c7ee6d9bb8141f4a5
Vikash Chuahan <102403038+Dario127@users.noreply.github.com>
2023-01-14 22:19:37 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,10 +1,41 @@
-resource "local_file" "pet" {
-	filename = var.filename
-	content  = var.content
+
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAWQHQ3XCLMB2OL5TN"
+  secret_key = "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"
 }
 
-resource "random_pet" "my-pet" {
-	prefix = var.prefix
-	separator = var.separator
-	length = var.length
+resource "aws_instance" "ubuntu" {
+  ami =   "ami-06878d265978313ca"
+  count = 1
+  instance_type = "t2.micro"
+
+  tags = {
+     name = "myfirstinstance"
+
+  }
 }
+
+resource "aws_security_group" "security_jenkins_port" {
+  name = "security_jenkins_port"
+  description = "SG for jenkins"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+    tags= {
+    name= "security_jenkins_group"
+  }
+}
+}
+
